765458f0d5115212ef8d675174f9c2d624c608e4
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-12 21:46:29 -0400

    more javascript, prepending Jquery div to new gif

--- assets/javascript/GifTastic.js
+++ assets/javascript/GifTastic.js
@@ -1,45 +1,50 @@
-$(document).ready(function() {
-var gifs = ["family guy", "the office", "Parks and Rec"];
+$(document).ready(function () {
+    var gifs = ["family guy", "the office", "Parks and Rec"];
 
-function displayGif() {
-    var gif = $(this).attr("data-gif")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9&q=&limit=25&offset=0&rating=G&lang=en" + searchTerm + "&api-key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        for (var j = 0; j < results.gifs; i++) {
-            var gifDiv = $("<div>");
-        var gifDisplay = response.gif;
+    function renderButton() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < gifs.length; i++) {
+            var newGif = $("<button>");
+            newGif.addClass("gif-btn");
+            newGif.attr("data-gif", gifs[i]);
+            newGif.text(gifs[i]);
+            newGif.addClass("btn");
+            $("#buttons-view").append(newGif);
+            console.log(newGif);
         }
-    })
-
-
-    $("#.btn-search").on("click", function () {
-        $("#gif-display").empty();
-        var searchTerm = $('submit').val();
-    })
-}
-
-function renderButton() {
-    $("#buttons-view").empty();
-    for (var i = 0; i < gifs.length; i++) {
-        var newGif = $("<button>");
-        newGif.addClass("gif-btn");
-        newGif.attr("data-gif", gifs[i]);
-        newGif.text(gifs[i]);
-        newGif.addClass("btn");
-        $("#buttons-view").append(newGif);
-        console.log(newGif);
     }
-}
 
-// $("#add-gif").on("click", function (event) {
-//     event.preventDefault();
-//     var input = $("#user-input").val().trim();
-//     gifs.push(input);
-//     renderButton();
-// })
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
+        var gif = $("#gif-input").val().trim();
+        gifs.push(gif);
+    })
+    renderButton();
 
-renderButton();
+
+    $(document).on("click", "button", function() {
+        var gif = $(this).attr("data-gif")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9&q=&limit=25&offset=0&rating=G&lang=en" + searchTerm + "&api-key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var gifDisplay = response.data;
+            for (var j = 0; j < gifs.length; i++) {
+                var gifsDiv = $("<div>");
+                var p = $("<p>").text("gifsDiv" + gifsDiv);
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+                
+                var newGif = ["gif1", "gif2"]
+                gifImage.addClass(newGif[i]);
+
+                gifsDiv.prepend(p);
+                gifsDiv.prepend(gifImage);
+                $("#gifs-appear-here").prepend(gifsDiv)l
+
+            }
+        })
+
+
 });
