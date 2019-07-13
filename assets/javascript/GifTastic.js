7c9d81bd19b7871e3ee7c9bc667d51c7927dbc43
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-12 22:42:23 -0400

    The userInput for the search bar is displaying it in a new button above it

--- assets/javascript/GifTastic.js
+++ assets/javascript/GifTastic.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    var gifs = ["family guy", "the office", "Parks and Rec"];
+    var gifs = ["family guy", "the office", "parks and rec"];
 
     function renderButton() {
         $("#buttons-view").empty();
         for (var i = 0; i < gifs.length; i++) {
             var newGif = $("<button>");
             newGif.addClass("gif-btn");
-            newGif.attr("data-info", gifs[i]);
+            newGif.attr("data-gif", gifs[i]);
             newGif.text(gifs[i]);
             newGif.addClass("btn");
             $("#buttons-view").append(newGif);
+            
             console.log(newGif);
         }
     }
@@ -23,9 +24,10 @@ $(document).ready(function () {
     });
 
     renderButton();
+    //Can't get the gifs to generate based on the value of the buttons.
     $(document).on("click", "button", function () {
-        var gif = $(this).attr("data-info")
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9&q=&limit=25&offset=0&rating=G&lang=en" + gif + "&api-key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9&limit=10";
+        var gif = $(this).attr("buttons-view")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api-key=NDDGg93an2SN1k7cICHE2VEWVhPV62X9&limit=10";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -35,12 +37,14 @@ $(document).ready(function () {
             for (var j = 0; j < results.length; j++) {
                 var gifsDiv = $("<div>");
                 var rating = results[j].rating;
-                var p = $("<p>").text("Rating" + rating);
+                var p = $("<p>").text("Rating:" + rating);
                 var gifImage = $("<img>");
                 gifImage.attr("src", results[j].images.fixed_height.url);
+                console.log(gifImage);
 
-                var gifName = ["gif1", "gif2",]
+                var gifName = ["first gif", "second gif", "first gif", "second gif"]
                 gifImage.addClass(gifName[j]);
+                console.log(gifName)
 
                 gifsDiv.prepend(p);
                 gifsDiv.prepend(gifImage);
